3838db3ad3e0f529edcba7198982645013c1f50d
Carlos Moreno <camorpe@gmail.com>
2023-03-07 12:45:07 +0100

    Commit inicial

---
+++ src/main/java/com/example/demo/LibreriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class LibreriaApplication {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public static void prueba() {
+		System.out.println("Prueba de librería");
+	}
+
+}
